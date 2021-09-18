cf9b642df7c6ad914da1980c12e07e95912ce3ce
SBaca <senior.baca@gmail.com>
2021-09-18 13:31:45 -0600

    Adding in Spring Initializr

---
+++ ChuckNorrisQuotes/src/main/java/guru/springframework/norris/chuck/ChuckNorrisQuotes/ChuckNorrisQuotesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisQuotesApplication.class, args);
+	}
+
+}
---
+++ ChuckNorrisQuotes/src/test/java/guru/springframework/norris/chuck/ChuckNorrisQuotes/ChuckNorrisQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
